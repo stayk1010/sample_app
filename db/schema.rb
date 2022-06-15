97c7f1621859d5ae3c9d79566bd60ff2f825abff
EC2 Default User <ec2-user@ip-172-31-31-189.ap-northeast-1.compute.internal>
2022-06-15 14:28:12 +0000

    [Finish]編集機能の作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_11_135059) do
+ActiveRecord::Schema.define(version: 2022_06_15_110510) do
 
   create_table "lists", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "body"
   end
 
 end
